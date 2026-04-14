5aeabb2dfb64d3967d20f2030c433c5e7e616848
yansongda <me@yansongda.cn>
2026-04-14 09:53:15 +0800

    feat: add built-in plugins

---
+++ src/plugins/mod.rs
@@ -0,0 +1,11 @@
+mod start;
+mod add_payload_body;
+mod add_radar;
+mod parser;
+mod log;
+
+pub use start::StartPlugin;
+pub use add_payload_body::AddPayloadBodyPlugin;
+pub use add_radar::AddRadarPlugin;
+pub use parser::ParserPlugin;
+pub use log::LogPlugin;
