1d9f7d0c838f10efe6cb1e77bfeb228733e32177
yansongda <me@yansongda.cn>
2026-04-14 09:45:05 +0800

    feat: add plugin trait and flow_ctrl

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ pub mod packer;
 pub mod packers;
 pub mod config;
 pub mod http;
+pub mod plugin;
+pub mod flow_ctrl;
 
 pub use error::{ArtfulError, Result};
 pub use direction::{Direction, DirectionKind, Destination};
@@ -13,3 +15,5 @@ pub use packer::Packer;
 pub use packers::JsonPacker;
 pub use config::{Config, LoggerConfig};
 pub use http::get_client;
+pub use plugin::Plugin;
+pub use flow_ctrl::{FlowCtrl, Next};
